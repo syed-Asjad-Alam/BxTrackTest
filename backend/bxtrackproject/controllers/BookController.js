68c8d575b85166ba973e8d8e81f6c40d2b0cada1
syed-Asjad-Alam <asjadakmal1@gmail.com>
2023-02-03 04:30:53 +0500

    view all, update and delete book completed and tested with postman

--- backend/bxtrackproject/controllers/BookController.js
+++ backend/bxtrackproject/controllers/BookController.js
@@ -1,5 +1,5 @@
 const Book = require('../models/BookModel')
-
+//for creating a book entity
 exports.createbook = async (req, res) => {
     try {
         const book = new Book({
@@ -16,6 +16,84 @@ exports.createbook = async (req, res) => {
             })
         )
 
+    }
+
+    catch (err){
+        res.status(500).json({
+            msg:"error"
+        })
+        return
+    }
+}
+//for getting a single book
+exports.getbook = async (req, res) => {
+    try {
+        const book = await Book.findOne({_id:req.params.id})
+        res.status(200).json({
+            status:"Success",
+            data: book
+            })
+ 
+    }
+
+    catch (err){
+        res.status(500).json({
+            msg:"error"
+        })
+        return
+    }
+}
+//for getting all books
+exports.getallbooks = async (req, res) => {
+    try {
+        const book = await Book.find()
+        res.status(200).json({
+            status:"Success",
+            items:book.length,
+            data: book
+            })
+ 
+    }
+
+    catch (err){
+        res.status(500).json({
+            msg:"error"
+        })
+        return
+    }
+}
+//for updating a book
+exports.updatebook = async (req, res) => {
+    try {
+        const book = await Book.findByIdAndUpdate({_id:req.params.id},{
+            title:req.body.title,
+            author:req.body.author,
+            pages:req.body.pages,
+            published_at:req.body.published_at
+        })
+        res.status(200).json({
+            status:"Updated Successfully",
+            data: book
+            })
+ 
+    }
+
+    catch (err){
+        res.status(500).json({
+            msg:"error"
+        })
+        return
+    }
+}
+//for deleting a book
+exports.deletebook = async (req, res) => {
+    try {
+        const book = await Book.deleteOne({_id:req.params.id})
+        res.status(200).json({
+            status:"Deleted Successfully",
+            data: book
+            })
+ 
     }
 
     catch (err){
